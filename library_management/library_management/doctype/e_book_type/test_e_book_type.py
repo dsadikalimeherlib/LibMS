f9894ec86c158c4fde324cd64d1d70b31216f229
RamjanAli <ramjanalilal143@gmail.com>
2023-04-04 13:22:49 +0000

    Add Master

---
+++ library_management/library_management/doctype/e_book_type/test_e_book_type.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, ramjanali and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestEBookType(FrappeTestCase):
+	pass
