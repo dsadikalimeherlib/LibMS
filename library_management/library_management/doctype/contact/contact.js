a9119778071b4cba6ad315b6b5345d50c3ba1199
ramjanalilal <ramjanalilal143@gmail.com>
2024-01-12 06:37:40 +0000

    Deleted File

--- library_management/library_management/doctype/contact/contact.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, ramjanali and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Contact', {
-	// refresh: function(frm) {
-
-	// }
-});
--- library_management/library_management/doctype/contact/test_contact.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023, ramjanali and Contributors
-# See license.txt
-
-# import frappe
-import unittest
-
-class TestContact(unittest.TestCase):
-	pass
