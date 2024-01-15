9b22da3ba6a641d7f7d3e09a0e597847bc8391e4
ramjanalilal <ramjanalilal143@gmail.com>
2024-01-15 11:26:12 +0000

    Membership Date & Sales Invoice

--- library_management/library_management/doctype/member/member.js
+++ library_management/library_management/doctype/member/member.js
@@ -2,22 +2,27 @@
 // For license information, please see license.txt
 
 //frappe.ui.form.on('Member', {
-//	add_address: function(frm) {
-            // Your client-side logic here
-//            frappe.msgprint("Button Clicked!");
-//        }
-//});
-frappe.ui.form.on('Member', {
-    add_address: function (frm) {
+//    refresh:function (frm) {
+
+//	}
         // Trigger the custom function when the button is clicked
-            openNewDocType(frm);
-    }
-});
+           //openNewDocType(frm);
+//});
 // Your custom function to open the new DocType
 function openNewDocType(frm) {
     frappe.route_options = {
         // Set any default values for the new DocType fields here
-        "address_title": frm.doc.name
+        Customer : frm.doc.member_name
     };
-    frappe.new_doc("Address");
+    frappe.new_doc("Customer");
 }
+frappe.ui.form.on('Member', {
+    refresh: function(frm) {
+        frm.add_custom_button('Customer', () => {
+            frappe.new_doc('Customer', {
+               custom_member : "abc"
+            })
+		//openNewDocType(frm);
+        })
+    }
+});
--- library_management/library_management/doctype/membership/membership.js
+++ library_management/library_management/doctype/membership/membership.js
@@ -5,4 +5,14 @@ frappe.ui.form.on('Membership', {
 	// refresh: function(frm) {
 
 	// }
+	membership_type: function(frm) {
+        // Get the current date
+        var From = frappe.datetime.get_today();
+
+        // Calculate another date based on the current date (e.g., 30 days later)
+        var To = frappe.datetime.add_days(From, frm.doc.days);
+
+        // Set the calculated date in another field (e.g., 'another_date_field')
+        frm.set_value('to_date', To);
+    }
 });
