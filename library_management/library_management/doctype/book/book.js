c38491fb9cb5a21e566baca55e7f12377c1396c6
ramjanalilal <ramjanalilal143@gmail.com>
2024-02-14 05:51:37 +0000

    New Update 1

--- library_management/library_management/doctype/book/book.js
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2024, ramjanali and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Book', {
-    // refresh: function(frm) {
-    // }
-});
-
-
--- library_management/library_management/doctype/book/test_book.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, ramjanali and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestBook(FrappeTestCase):
-	pass
