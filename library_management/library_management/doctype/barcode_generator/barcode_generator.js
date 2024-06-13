115d03e60678e41c5b012bea04594d90183e8fd7
RamjanAli Lal <ramjanalilal@RamjanAlis-MacBook-Pro.local>
2024-06-13 17:11:47 +0530

    Barcode Print

--- library_management/library_management/doctype/barcode_generator/barcode_generator.js
+++ library_management/library_management/doctype/barcode_generator/barcode_generator.js
@@ -1,8 +1,32 @@
 // Copyright (c) 2024, ramjanali and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Barcode Generator", {
-// 	refresh(frm) {
+frappe.ui.form.on('Barcode Generator', {
+    item_code: function(frm) {
+        if (frm.doc.item_code) {
+            // Fetch all assets related to the given item code
+            frappe.db.get_list('Asset', {
+                filters: { item_code: frm.doc.item_code },
+                fields: ['name','asset_name','status']
+            }).then(assets => {
+                // Clear existing data in the child table
+                frm.clear_table('barcode_generator_details');
 
-// 	},
-// });
+                // Add a new row for each asset related to the item code to the child table
+                assets.forEach(asset => {
+                    let row = frappe.model.add_child(frm.doc, 'Barcode Generator Details', 'barcode_generator_details');
+                    row.asset = asset.name;
+                    row.asset_name = asset.asset_name;
+                    row.status = asset.status;
+                });
+
+                // Refresh the child table
+                frm.refresh_field('barcode_generator_details');
+
+                // Optional: Show a message
+                frappe.msgprint(__('Fetched {0} assets', [assets.length]));
+            });
+        }
+    }
+});
+
--- library_management/library_management/doctype/barcode_generator/barcode_generator.py
+++ library_management/library_management/doctype/barcode_generator/barcode_generator.py
@@ -1,9 +1,10 @@
 # Copyright (c) 2024, ramjanali and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class BarcodeGenerator(Document):
 	pass
+
