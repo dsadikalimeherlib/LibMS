8ceca614932c56b34c23dac953d93062adca94b2
ramjanalilal <ramjanalilal143@gmail.com>
2024-08-07 09:59:15 +0000

    Item Auto Create bu Book and asset naming series field is from Library Setting

--- library_management/library_management/doctype/book/book.py
+++ library_management/library_management/doctype/book/book.py
@@ -10,17 +10,15 @@ class Book(WebsiteGenerator):
         # self.create_item_from_book()
         #frappe.msgprint("Validation successful")
         self.create_item_from_book()
-
-    # def sample(self):
-    #     if self.is_new():
-    #         frappe.msgprint("Book is new")
-    #         self.create_item_from_book()
         
     def create_item_from_book(self):
         if self.is_new():
             frappe.msgprint("Creating Item from Book")
             category = frappe.get_value('Library Setting', 'default_asset_category','default_asset_category')
-            #category = frappe.get_value('Library Setting', filters={'name': 'default_asset_category'}, fieldname='default_asset_category')
+            asset_naming = frappe.get_value('Library Setting', 'asset_naming_series','asset_naming_series')
+            if not category:
+                frappe.throw("Default asset category is not set in Library Settings.")
+                
             item_doc = frappe.get_doc({
                 'doctype': 'Item',
                 'item_code': self.name,
@@ -30,9 +28,10 @@ class Book(WebsiteGenerator):
                 'include_item_in_manufacturing': 0,
                 'is_fixed_asset': 1,
                 'auto_create_assets': 1,
+                'asset_naming_series': asset_naming,
                 'asset_category': category
             })
             item_doc.insert()
-            #self.item_name = item_doc.name
+            self.item_name = item_doc.name
             frappe.msgprint("Item created successfully")
 
