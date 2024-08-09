43df591d5578abb7910e2d32d81c839b24d52a3c
ramjanalilal <ramjanalilal143@gmail.com>
2024-08-09 14:25:05 +0000

    Banner & Multimedia Doctype Created

---
+++ library_management/library_management/doctype/multimedia_category/multimedia_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, ramjanali and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Multimedia Category", {
+// 	refresh(frm) {
+
+// 	},
+// });
