7c60ffc9d9ce6be249b50cafdf1ce9542f862827
dsadikalimeherlib <sadikalidantreleeya@meherlibrary.org>
2024-09-03 21:30:25 +0530

    add slider

--- library_management/public/js/ebook_reader/components/home/banner-slider/BannerSlider.vue
+++ library_management/public/js/ebook_reader/components/home/banner-slider/BannerSlider.vue
@@ -2,52 +2,72 @@
 <template>
   <div class="banner-section">
     <div class="container">
-      <div class="flexslider">
-        <ul class="slides">
-          <li>
-            <img src="/files/banner.png" />
-          </li>
-          <!-- <li>
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/5689/grooves.jpg" />
-            </li>
-            <li>
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/5689/arch.jpg" />
-            </li>
-            <li>
-                <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/5689/sunset.jpg" />
-            </li> -->
-        </ul>
+      <div class="slider-container">
+        <div class="slider">
+          <!-- Display each banner -->
+          <div v-for="(banner, index) in banners" :key="index" class="slide"
+            :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
+            <img :src="banner.image" :alt="banner.title" />
+          </div>
+        </div>
+
+
+        <!-- Pagination Dots -->
+
+      </div>
+      <div class="pagination-dots">
+        <span v-for="(banner, index) in banners" :key="index" class="dot" :class="{ active: index === currentIndex }"
+          @click="goToSlide(index)"></span>
       </div>
     </div>
   </div>
 
+
+
 </template>
 <script>
-// // define slider component
-// Vue.component('img-slider', {
-//     template: '#img-slider-template',
-//     replace: true
-// })
-// // boot up demo
-// new Vue({
-//     el: '#demo'
-// })
-</script>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-<script defer src="http://flexslider.woothemes.com/js/jquery.flexslider.js"></script>
-<script>
-$(function(){
-  SyntaxHighlighter.all();
-});
-$(window).load(function(){
-  $('.flexslider').flexslider({
-    animation: "slide",
-    start: function(slider){
-      $('body').removeClass('loading');
+export default {
+  data() {
+    return {
+      banners: [
+        { image: "/files/banner.png", title: "Banner 1" },
+        { image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/5689/grooves.jpg", title: "Banner 2" },
+        { image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/5689/sunset.jpg", title: "Banner 3" }
+      ],
+      currentIndex: 0,
+      autoSlideInterval: null // To hold the interval reference for auto-sliding
+    };
+  },
+  methods: {
+    nextSlide() {
+      this.currentIndex = (this.currentIndex + 1) % this.banners.length; // Loop back to the start
+    },
+    prevSlide() {
+      this.currentIndex =
+        (this.currentIndex - 1 + this.banners.length) % this.banners.length; // Loop back to the end
+    },
+    goToSlide(index) {
+      this.currentIndex = index;
+      this.resetAutoSlide(); // Reset the auto-slide when user manually selects a slide
+    },
+    startAutoSlide() {
+      this.autoSlideInterval = setInterval(this.nextSlide, 3000); // Auto-slide every 3 seconds
+    },
+    resetAutoSlide() {
+      clearInterval(this.autoSlideInterval);
+      this.startAutoSlide();
     }
-  });
-});
+  },
+  mounted() {
+    this.startAutoSlide(); // Start auto-sliding when the component is mounted
+  },
+  beforeDestroy() {
+    clearInterval(this.autoSlideInterval); // Clean up the interval when the component is destroyed
+  }
+};
 </script>
+
+<style scoped></style>
 <style scoped>
 @import "./style.css";
 </style>
