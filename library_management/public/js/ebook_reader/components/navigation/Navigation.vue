4328da50ae6b31ca7cf3de93198bba9316e32248
dsadikalimeherlib <sadikalidantreleeya@meherlibrary.org>
2024-09-08 14:55:59 +0530

    Update navigation

--- library_management/public/js/ebook_reader/components/navigation/Navigation.vue
+++ library_management/public/js/ebook_reader/components/navigation/Navigation.vue
@@ -3,9 +3,9 @@
         <button @click="handleClick('book-categories')">Book categories</button>
         <button @click="handleClick('media-categories')">Multimedia Categories</button>
         <button @click="handleClick('about')">Apply Membership</button>
-        <button @click="handleClick('about')">Fees</button>
-        <button @click="handleClick('about')">News & Announcements</button>
-        <button @click="handleClick('about')">Holidays</button>
+        <button @click="handleClick('fees')">Fees</button>
+        <button @click="handleClick('news-announcement')">News & Announcements</button>
+        <button @click="handleClick('holidays')">Holidays</button>
         <button @click="handleClick('about')">Contact us</button>
         <button @click="handleClick('about')">Feedback</button>
     </div>
